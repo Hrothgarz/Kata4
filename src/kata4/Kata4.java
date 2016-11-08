95f8e9c30b3e3fbf74c8b1a9d0285e4d0bbd7c71
Hrothgarz <Hrothgarz@Hrothgarz-PC>
2016-11-08 02:21:29 +0000

    Primera versión de Kata4v3

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,12 +1,17 @@
 package kata4;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        File file = new File("D:\\Nueva Carpeta");
-        print(file.listFiles()," ");
+    public static void main(String[] args) throws IOException {
+        String filename = "D:\\NuevaCarpeta/emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(filename);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
     private static void print(File[] files, String indent){
